85daeaf6715e9325953ebe7fa65e9eccdd8ce459
jordan <jharrison1@qa.com>
2022-07-05 12:27:09 +0100

    updated with logic

--- src/main/java/com/qa/may/rest/DinoController.java
+++ src/main/java/com/qa/may/rest/DinoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.websocket.server.PathParam;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -32,25 +34,47 @@ public class DinoController {
 		return new Dinosaur("Terry", "T-Rex", Integer.MAX_VALUE);
 	}
 
+	@GetMapping("/getDino/{id}")
+	public Dinosaur getById(@PathVariable int id) {
+		return this.dinos.get(id);
+	}
+
+	@GetMapping("/getDinos")
+	public List<Dinosaur> getAll() {
+		return this.dinos;
+	}
+
 	@PostMapping("/createDino")
-	public Dinosaur create(@RequestBody Dinosaur dino) {
+	public ResponseEntity<Dinosaur> create(@RequestBody Dinosaur dino) {
 		System.out.println("Created: " + dino);
 		this.dinos.add(dino);
-		return this.dinos.get(this.dinos.size() - 1);
+		Dinosaur created = this.dinos.get(this.dinos.size() - 1);
+
+		return new ResponseEntity<Dinosaur>(created, HttpStatus.CREATED);
 	}
 
 	@PatchMapping("/updateDino/{id}")
-	public void update(@PathVariable("id") int id, @PathParam("name") String name, @PathParam("species") String species,
-			@PathParam("age") int age) {
+	public Dinosaur update(@PathVariable("id") int id, @PathParam("name") String name,
+			@PathParam("species") String species, @PathParam("age") Integer age) {
 		System.out.println("ID: " + id);
 		System.out.println("Name: " + name);
 		System.out.println("Species: " + species);
 		System.out.println("Age: " + age);
+
+		Dinosaur toUpdate = this.dinos.get(id);
+		if (name != null)
+			toUpdate.setName(name);
+		if (age != null)
+			toUpdate.setAge(age);
+		if (species != null)
+			toUpdate.setSpecies(species);
+		return toUpdate;
 	}
 
 	// id = 4494
 	@DeleteMapping("/removeDino/{id}")
-	public void delete(@PathVariable int id) {
+	public ResponseEntity<?> delete(@PathVariable int id) {
 		System.out.println("ID: " + id);
+		this.dinos.remove(id);
 	}
 }
